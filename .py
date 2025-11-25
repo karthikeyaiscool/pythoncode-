0ea71efafe8aab5cc8b95856dea0dc31222a767f
karthikeyaiscool <kishortvs@gmail.com>
2025-11-24 20:30:53 -0500

    Add files via upload

--- .py
+++ .py
@@ -1,6 +1,13 @@
-def prints(n):
-    if("n<=0"):
-        return
-    print("codingal")
-    prints(n/2)
-    prints(n/2)
+def arraysum(a):
+    sum=0
+    for i in a:
+
+        sum = sum + 1
+
+    return(sum)
+a = [12,3,4,15]
+arraysum(a)
+def summ(n):
+    if(n<=0):
+       return
+    return n+ summ(n-1)

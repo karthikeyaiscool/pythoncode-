599fb4e530c8a57d3e25bdffc5e41b9eb9af9098
karthikeyaiscool <kishortvs@gmail.com>
2025-12-01 20:14:52 -0500

    Add files via upload

--- .py
+++ .py
@@ -1,13 +1,19 @@
-def arraysum(a):
-    sum=0
-    for i in a:
+# program to find if a number is an armstrong number
 
-        sum = sum + 1
-
-    return(sum)
-a = [12,3,4,15]
-arraysum(a)
-def summ(n):
-    if(n<=0):
-       return
-    return n+ summ(n-1)
+# take input from the user
+number = int(input("input your number:"))
+# calculate number of digits
+digits = len(str(number))
+# initalize result variable
+resultnumber = 0
+# find sum of the a^digits of each digit 
+temp = number 
+while temp > 0:
+    digit = temp % 10
+    resultnumber += digit ** digits
+    temp //= 10
+# display the result
+if number == resultnumber:
+   print(number,"is an armstrong number")
+else:
+   print(number,"is not an armstrong number")
